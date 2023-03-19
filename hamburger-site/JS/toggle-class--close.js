5fa13c44ec6412ad47aa068894dafa069a763cd4
Tsutomu <tutokun0723@gmail.com>
2023-03-19 23:00:57 +0900

    検索ボタン、タグ見直し、サイドメニューの挙動を調整

---
+++ hamburger-site/JS/toggle-class--close.js
@@ -0,0 +1,12 @@
+
+
+
+// サイドメニューの×ボタンを押したらis-activクラスを外す
+$(function(){
+  $('.c-button-close').on({'click' : function(){
+    $('.c-color-board--black').toggleClass('is-active'),
+    $('.c-slide--button').toggleClass('is-active'),
+    $('.l-aside').toggleClass('is-active');
+    }
+  });
+});
